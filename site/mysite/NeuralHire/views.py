520b26085d17c5352b2efff381e2819614df694b
Nursemen <nnursultan07@mail.ru>
2023-01-28 12:14:06 +0600

    all frontend

--- site/mysite/NeuralHire/views.py
+++ site/mysite/NeuralHire/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 def main(request):
-    return render(request, 'neuralhire/index.html', {})
+    list_of_additions = ['Опыт не нужен', 'Доступно студентам',
+                        'Доступно для соискателей от 45+ лет', 'Доступно для соискателей с ограниченными возможностями',
+                        'Удаленная работа', 'Отклик без резюме']
+    return render(request, 'neuralhire/index.html', {'additions':list_of_additions})
